f21cef41dd0c940d20cd6b49b7a859e923b09247
FrosT2k5 <yashpatil2k5@gmail.com>
2024-09-23 20:50:46 +0530

    app.js: Introduce Swagger UI API Docs run: npm run swagger to rebuild docs
    Signed-off-by: FrosT2k5 <yashpatil2k5@gmail.com>

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const swaggerUi = require('swagger-ui-express');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,6 +13,7 @@ var investmentRouter = require('./routes/investments');
 var expenseTrackerRouter = require('./routes/expensetracker');
 
 var app = express();
+const swaggerDocument = require('./swagger-output.json');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -29,7 +31,7 @@ app.use('/api/login', loginRouter);
 app.use('/api/register', registerRouter);
 app.use('/api', investmentRouter);
 app.use('/api/expensetracker', expenseTrackerRouter);
-
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
