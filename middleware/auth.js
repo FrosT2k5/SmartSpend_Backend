33bd731460c0300a1faf886aaebab29a6c20056d
FrosT2k5 <yashpatil2k5@gmail.com>
2024-10-06 17:17:10 +0530

    auth: respond with 403 status code in case of invalid token
    Signed-off-by: FrosT2k5 <yashpatil2k5@gmail.com>

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,7 +6,7 @@ const verifyToken = (req, res, next) => {
     const token = authHeader && authHeader.split(' ')[1];
 
     if (!token) {
-        return res.status(403).json({ message: 'No token provided' });
+        return res.status(401).json({ message: 'No token provided' });
     }
 
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
